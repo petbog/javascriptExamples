2fe5e008d6d4e37700844d7f8d4d70393b6d79f3
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-06-13 23:24:33 +0300

    решаю

--- script2.js
+++ script2.js
@@ -84,13 +84,86 @@
 
 //Даны два слова. Проверьте, что последняя буква первого слова совпадает с первой буквой второго слова.
 
-const stringResult = (str) => {
-    const string1 = str.split(' ')
-    const [arr1, arr2] = string1
-    if (arr1.slice(-1) === arr2.slice(0,1)) {
-        console.log('совпадают')
-    } else {
-        console.log('не совпадают')
-    }
-}
-stringResult('привет том')
+// const stringResult = (str) => {
+//     const string1 = str.split(' ')
+//     const [arr1, arr2] = string1
+//     if (arr1.slice(-1) === arr2.slice(0,1)) {
+//         console.log('совпадают')
+//     } else {
+//         console.log('не совпадают')
+//     }
+// }
+// stringResult('привет том')
+
+//Дана некоторая строка. Найдите позицию третьего нуля в строке.
+
+// const nullString = (str) => {
+//     let nullStr = 0
+//     const arr = []
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === '0') {
+//             nullStr++
+//             if(nullStr === 3){
+//                 arr.push(i)
+//             }
+//         }
+//     }
+//     console.log(arr)
+// }
+
+// nullString('012301230')
+
+//Даны числа, разделенные запятыми:
+
+// '12,34,56'
+// Найдите сумму этих чисел.
+
+// const sumNumbers = (str) => {
+// const result=str.split(',').map(Number)
+// const arr=result.reduce((acc,item)=>acc + item,0)
+// console.log(arr)
+// }
+// sumNumbers('12,34,56')
+
+// Дана дата в следующем формате:
+
+// '2025-12-31'
+// Преобразуйте эту дату в следующий объект:
+
+// {
+// 	year: '2025',
+// 	month: '12',
+// 	day: '31',
+// }
+
+// const data = (num) => {
+//     const [year, month, day] = num.split('-')
+//     const dateObj = { year, month, day };
+//     console.log(year, month, day)
+// }
+
+// data('2025 - 12 - 31')
+
+//Дана некоторая строка с буквами и цифрами. Получите позицию первой цифры в этой строке.
+
+// const searchNumber=(srt)=>{
+//     const numEl = parseInt(srt.match(/\d+/))
+//     console.log(numEl)
+// }
+
+// searchNumber('asdasdas2')
+
+//Дан объект с ключами и значениями. Запишите в первый массив ключи объекта, а во второй - значения.
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
+// const values = (obj) => {
+// const key=Object.keys(obj)
+// const values=Object.values(obj)
+// console.log(key)
+// console.log(values)
+// }
+// values(user)
+
+//Дано число. Выведите в консоль количество четных цифр в этом числе.
